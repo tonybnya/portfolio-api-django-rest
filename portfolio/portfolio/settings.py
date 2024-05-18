583692a88c267cba827c77d37daa73b804099aa6
tonybnya <nya.tony2010@gmail.com>
2024-05-18 12:46:49 +0100

    fix: set the DJANGP_SECRET_KEY environment variable

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -35,7 +35,7 @@ def get_env_variable(var_name):
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable("/DJANGO_SECRET_KEY")
+SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable("DJANGO_DEBUG") == "True"
