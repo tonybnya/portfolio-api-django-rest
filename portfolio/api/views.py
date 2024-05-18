b7068d3f14be95bf1df8dfc43a52c0067019aee4
tonybnya <nya.tony2010@gmail.com>
2024-05-18 07:45:12 +0100

    feat: create viewsets for my models

--- portfolio/api/views.py
+++ portfolio/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Project, ProjectImage, Tag
+from .serializers import ProjectImageSerializer, ProjectSerializer, TagSerializer
+
 
 # Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class ProjectImageViewSet(viewsets.ModelViewSet):
+    queryset = ProjectImage.objects.all()
+    serializer_class = ProjectImageSerializer
